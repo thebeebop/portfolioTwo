5072cfadb45ee1a96bb57e43d5c6ca871fb0ba1f
Lewis Woods <thebebop@The-Beebop.local>
2023-01-24 17:49:38 +0000

    Finished making the weather-widget-componenet. Also designed the layout.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,19 +2,23 @@ import Header from "./components/header";
 import Projects from "./components/projects";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import WeatherWidget from "./components/projects/WeatherWidget";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<Projects />}></Route>
-          <Route path="about" element={<About />}></Route>
-          <Route path="contact" element={<Contact />}></Route>
-        </Routes>
-      </div>
+
+      <Routes>
+        <Route path="/" element={<Projects />}></Route>
+        <Route
+          path="/projects/weather-widget"
+          element={<WeatherWidget />}
+        ></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="contact" element={<Contact />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -3,14 +3,16 @@ import "../style/contact.css";
 export default function Contact() {
   return (
     <>
-      <h1>Thank you kindly for visiting my site!</h1>
-      <div className="contact-info-container">
-        <div className="card-small">Email:</div>
-        <div className="card-small">PhoneNumber:</div>
-      </div>
-      <div className="contact-info-container-two">
-        <div className="card-small">Li</div>
-        <div className="card-small">Github</div>
+      <div className="app">
+        <h1>Thank you kindly for visiting my site!</h1>
+        <div className="contact-info-container">
+          <div className="card-small">Email:</div>
+          <div className="card-small">PhoneNumber:</div>
+        </div>
+        <div className="contact-info-container-two">
+          <div className="card-small">Li</div>
+          <div className="card-small">Github</div>
+        </div>
       </div>
     </>
   );
